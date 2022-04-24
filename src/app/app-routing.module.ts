9e259384e59e7bda769605a3e8a14855df344f31
Dzmitry Shchapiorka <dzmitry_shchapiorka@epam.com>
2022-04-24 19:16:35 +0200

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { HomePageComponent } from './core/pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'boards',
+    loadChildren: () => import('./boards/boards.module').then((m) => m.BoardsModule),
+    canLoad: [AuthGuard],
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'log-in',
+    component: LogInPageComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
