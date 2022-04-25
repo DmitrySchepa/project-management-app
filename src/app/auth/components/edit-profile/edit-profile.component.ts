626faddfe0cb681a0eac92e1c7f6bf1ba260012b
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-25 20:56:48 +0300

    feat: implement edit profile component extends sign up and base form components

---
+++ src/app/auth/components/edit-profile/edit-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss', '../formStyles.scss'],
+})
+export class EditProfileComponent extends SignUpComponent implements OnInit {
+  constructor(fb: FormBuilder, authService: AuthService) {
+    super(fb, authService);
+  }
+
+  override ngOnInit() {
+    super.ngOnInit();
+    // TODO: get userData and insert in form fields
+  }
+
+  override submit() {
+    if (this.formGroup.invalid) return;
+    this.authService.updateUser({
+      name: this.name?.value,
+      login: this.email?.value,
+      password: this.password?.value,
+    });
+  }
+}
