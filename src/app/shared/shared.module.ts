2ecb731979a32d01117b0f50816be71dafddbd6c
Dzmitry Shchapiorka <dzmitry_shchapiorka@epam.com>
2022-04-25 13:32:35 +0200

    feat: add header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
 
-const MaterialModules = [MatToolbarModule];
+const MaterialModules = [MatToolbarModule, MatIconModule, MatRadioModule];
 
 @NgModule({
   declarations: [],
