542fcb0e52c1eb765cb6c62963bd3ce6994725b8
Dzmitry Shchapiorka <dzmitry_shchapiorka@epam.com>
2022-04-25 16:08:14 +0200

    feat: add img folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-const MaterialModules = [MatToolbarModule, MatIconModule, MatRadioModule];
+const MaterialModules = [MatToolbarModule, MatIconModule, MatRadioModule, MatButtonToggleModule];
 
 @NgModule({
   declarations: [],
