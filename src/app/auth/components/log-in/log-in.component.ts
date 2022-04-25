f2fcb416ef9e840c34ed2189ebf5df9456a2a975
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-25 20:49:59 +0300

    feat: implement login component extends auth directive

--- src/app/auth/components/log-in/log-in.component.ts
+++ src/app/auth/components/log-in/log-in.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { AuthDirective } from '../../directives/auth.directive';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss'],
+  styleUrls: ['./log-in.component.scss', '../formStyles.scss'],
 })
-export class LogInComponent implements OnInit {
-  constructor() {}
+export class LogInComponent extends AuthDirective implements OnInit {
+  constructor(private readonly fb: FormBuilder, private readonly authService: AuthService) {
+    super();
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  submit() {
+    if (this.formGroup.invalid) return;
+    console.log('log');
+    this.authService.login(this.formGroup.value);
+  }
 }
