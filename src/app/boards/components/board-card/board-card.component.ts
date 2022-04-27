dd5efc8226d76c954824b695bf146171fb838a51
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-28 00:04:42 +0300

    feat: add click handlers

--- src/app/boards/components/board-card/board-card.component.ts
+++ src/app/boards/components/board-card/board-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { BoardModel } from '../../models/board.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-board-card',
@@ -8,4 +9,17 @@ import { BoardModel } from '../../models/board.model';
 })
 export class BoardCardComponent {
   @Input() board!: BoardModel;
+
+  constructor(private readonly router: Router) {}
+
+  openBoard(event: Event, id: string) {
+    const target = event.target as HTMLElement;
+    if (target.tagName !== 'MAT-CARD') return;
+    this.router.navigate(['main', 'board', id]);
+  }
+
+  deleteBoard(id: string) {
+    console.log('delete board', id);
+    // TODO call dialog, which gets type of elem (board, column, task), elem id
+  }
 }
