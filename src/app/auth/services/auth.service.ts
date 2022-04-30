c7b02a27a3e7360301aeae7cae9abc0e3aaa27fb
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-30 19:48:45 +0300

    feat: implements methods for interaction with api

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,15 +1,61 @@
 import { Injectable } from '@angular/core';
 import { LoginModel, UserModel } from '../models/auth.model';
+import { ApiService } from '../../core/services/api.service';
+import { concatAll, find, map, pluck, startWith, Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private readonly apiService: ApiService, private readonly router: Router) {}
 
-  login(login: LoginModel) {}
+  public isAuth$ = new Subject<Boolean>();
 
-  signup(user: UserModel) {}
+  public userData!: UserModel;
 
-  updateUser(user: UserModel) {}
+  checkAuth() {
+    const isAuth = !!localStorage.getItem('pwa-token');
+    return this.isAuth$.pipe(startWith(isAuth));
+  }
+
+  login(login: LoginModel) {
+    this.apiService
+      .login(login)
+      .pipe(pluck('token'))
+      .subscribe((token) => localStorage.setItem('pwa-token', token as string));
+    this.apiService
+      .getUsers()
+      .pipe(
+        concatAll(),
+        find((user) => user.login === login.login),
+        map((user) => user?.id as string),
+      )
+      .subscribe((user) => {
+        localStorage.setItem('pwa-user-id', user);
+        this.router.navigate(['main', 'boards']);
+      });
+  }
+
+  signup(user: UserModel) {
+    this.apiService.signup(user).subscribe(() => this.router.navigate(['auth', 'log-in']));
+  }
+
+  getUser() {
+    return this.apiService.getUser();
+  }
+
+  updateUser(user: UserModel) {
+    this.apiService.updateUser(user).subscribe(() => this.router.navigate(['main', 'boards']));
+  }
+
+  logout() {
+    localStorage.removeItem('pwa-token');
+    localStorage.removeItem('pwa-user-id');
+    this.router.navigate(['']);
+  }
+
+  deleteUser() {
+    this.apiService.deleteUser().subscribe(() => this.logout());
+  }
 }
