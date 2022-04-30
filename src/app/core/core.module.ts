789689a5e1de8699dd8ba6f11fe207940364f8b2
Nikolai Makarevich <nikoladze529@mail.ru>
2022-04-30 20:41:46 +0300

    feat: add router module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,10 +6,17 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomePageComponent, NotFoundComponent, NotFoundPageComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    NotFoundComponent,
+    NotFoundPageComponent,
+  ],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
