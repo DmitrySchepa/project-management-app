a55d502ba5a35f8dba47790320a7ab3bdecb37b8
natein <natein@gmail.com>
2022-05-01 03:13:36 +0300

    fix: remove code example

--- src/app/core/pages/home-page/home-page.component.ts
+++ src/app/core/pages/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ConfirmationDialogComponent } from '../../components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-home-page',
@@ -8,27 +6,7 @@ import { ConfirmationDialogComponent } from '../../components/confirmation-dialo
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  boardTitle = "PMA development"
-  constructor(private dialog: MatDialog ) { }
+  constructor() {}
 
   ngOnInit(): void {}
-
-  openDialog() {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent,{
-      data:{
-        message: `Are you sure want to delete board ${ this.boardTitle }?`,
-        buttonText: {
-          ok: 'Yes',
-          cancel: 'No'
-        }
-      }
-    });
-
-    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-      if (confirmed) {
-      // Здесь можно удалить 
-        console.log("confirmed!");
-      }
-    });
-  }
 }
