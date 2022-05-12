d02c533cdee62f232096c38d6151d8ab5e3779b6
Nikolai Makarevich <nikoladze529@mail.ru>
2022-05-12 23:47:29 +0300

    feat: fill board-task component

--- src/app/boards/components/board-task/board-task.component.ts
+++ src/app/boards/components/board-task/board-task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BoardTask } from '../../models/board.model';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../../../core/components/confirmation-dialog/confirmation-dialog.component';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-board-task',
@@ -6,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-task.component.scss'],
 })
 export class BoardTaskComponent implements OnInit {
-  constructor() {}
+  @Input() task!: BoardTask;
+
+  constructor(private readonly dialog: MatDialog, private readonly store: Store) {}
 
   ngOnInit(): void {}
+
+  deleteTask() {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        message: `Are you sure want to delete task '${this.task.title}'?`,
+        buttonText: {
+          ok: 'Delete',
+          cancel: 'Cancel',
+        },
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        console.log('delete task', this.task.id);
+        //this.store.dispatch(deleteTask({ id: this.task.id }));
+      }
+    });
+  }
 }
