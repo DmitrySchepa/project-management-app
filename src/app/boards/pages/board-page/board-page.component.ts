397b60d85d2469c532ba428b75cb9b9201815c53
Nikolai Makarevich <nikoladze529@mail.ru>
2022-05-12 12:18:43 +0300

    feat: add algorithm for reorder columns

--- src/app/boards/pages/board-page/board-page.component.ts
+++ src/app/boards/pages/board-page/board-page.component.ts
@@ -27,6 +27,23 @@ export class BoardPageComponent implements OnInit {
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+    this.updateColumns(event)
+  }
+
+  updateColumns(event: CdkDragDrop<string[]>) {
+    const { previousIndex, currentIndex, item, container } = event;
+    const columnId = item.element.nativeElement.dataset['id'];
+    const columns = (Array.from(container.element.nativeElement.children) as HTMLElement[])
+      .filter(item => item.tagName !== 'BUTTON');
+    if (previousIndex < currentIndex) {
+      for (let i = previousIndex + 1; i <= currentIndex; i += 1) {
+        console.log(columns[i], i) //tasks[i] order = i
+      }
+    } else {
+      for (let i = currentIndex; i < previousIndex; i += 1) {
+        console.log(columns[i], i + 2) // tasks[i] order = i + 2
+      }
+    }
   }
 
   onAddColumn() {
