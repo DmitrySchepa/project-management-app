944aa420ce3e77453657f4d02f91900cd4b2def2
Nikolai Makarevich <nikoladze529@mail.ru>
2022-05-29 12:38:38 +0300

    refactor: delete unused code

--- src/app/boards/components/board-participants/board-participants.component.ts
+++ src/app/boards/components/board-participants/board-participants.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { BoardModel, Participant } from '../../models/board.model';
-import { BoardsService } from '../../services/boards.service';
-// import { boardsMock } from 'src/mocks/board-model.mock';
+import { Component } from '@angular/core';
+import { Participant } from '../../models/board.model';
 
 @Component({
   selector: 'app-board-participants',
   templateUrl: './board-participants.component.html',
   styleUrls: ['./board-participants.component.scss'],
 })
-export class BoardParticipantsComponent implements OnInit {
+export class BoardParticipantsComponent {
   participants?: Participant[] = [];
-
-  // boards: BoardModel[] = boardsMock;
-
-  constructor(private boardsService: BoardsService) {}
-
-  ngOnInit(): void {
-    // this.boards.forEach((elem: BoardModel) => (this.participants = elem.participants));
-  }
 
   fakeParticipant = { name: 'John', email: '@joejohn' };
 
   onDeleteParticipant = (ind: number) => this.participants?.splice(ind, 1);
 
-  onAddParticipant = (patricipant: Participant) => this.participants?.push(patricipant);
+  onAddParticipant = (patrticipant: Participant) => this.participants?.push(patrticipant);
 }
--- src/app/boards/components/board-search/board-search.component.ts
+++ src/app/boards/components/board-search/board-search.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board-search',
   templateUrl: './board-search.component.html',
   styleUrls: ['./board-search.component.scss'],
 })
-export class BoardSearchComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class BoardSearchComponent {}
